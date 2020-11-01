b26b5240a58071c5eafbfee744f47ea0ac8d658d
Nikolay <nickolay.i.tkachenko@gmail.com>
2020-11-01 04:00:28 +0300

    Ver 03 incomplete (Add Network + Detail+Pallete)

--- app/src/main/java/com/rsh_engineering/tkachenkoni/atlasofanimals/view/DetailFragment.kt
+++ app/src/main/java/com/rsh_engineering/tkachenkoni/atlasofanimals/view/DetailFragment.kt
@@ -1,17 +1,23 @@
 package com.rsh_engineering.tkachenkoni.atlasofanimals.view
 
+import android.R.attr.bitmap
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
+import androidx.fragment.app.Fragment
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.rsh_engineering.tkachenkoni.atlasofanimals.R
 import com.rsh_engineering.tkachenkoni.atlasofanimals.model.AnimalModel
 import com.rsh_engineering.tkachenkoni.atlasofanimals.util.getProgressDrawable
 import com.rsh_engineering.tkachenkoni.atlasofanimals.util.loadImage
 import kotlinx.android.synthetic.main.fragment_detail.*
-import kotlinx.android.synthetic.main.fragment_list.*
 
 
 class DetailFragment : Fragment() {
@@ -44,6 +50,7 @@ class DetailFragment : Fragment() {
 
     fun initUI(){
         viewSetup()
+        viewSetupBackgroundColor()
     }
 
     fun viewSetup(){
@@ -54,4 +61,54 @@ class DetailFragment : Fragment() {
         tv_animal_diet.text = animal?.diet
     }
 
+    fun viewSetupBackgroundColor(){
+        animal?.imageUrl?.let { url ->
+            Log.d("COLORPALETTE", " url = $url")
+            Glide.with(this)
+                .asBitmap()
+                .load(url)
+                .into(object : CustomTarget<Bitmap>() {
+
+                    override fun onResourceReady(
+                        resource: Bitmap,
+                        transition: Transition<in Bitmap>?
+                    ) {
+
+                        Log.d("COLORPALETTE", " resource = $resource")
+                        Log.d("COLORPALETTE", " resource.byteCount = ${resource.byteCount}")
+
+//                        val palImg = Palette.from(resource).generate()
+//                        val intColor = palImg?.lightMutedSwatch?.rgb ?: 0
+
+//                        ll_animal_detail.setBackgroundColor(intColor)
+                        //Log.d("COLORPALETTE", " iniColor = $intColor")
+
+                        val palette = Palette.from(resource).generate()
+                        val defaultColor = -0xcccccd
+                        val color = palette.getDarkMutedColor(defaultColor)
+                        //ll_animal_detail.setBackgroundColor(color)
+
+
+                        //val imageViewTarget = GlideDrawableImageViewTarget(load)
+                        //val mutableBitmap = Bitmap.createBitmap(resource.width, resource.height, Bitmap.Config.RGB_565)
+
+//                        {palette ->
+//                                val intColor = palette?.lightMutedSwatch?.rgb ?: 0
+//                                Log.d("COLORPALETTE", " iniColor = $intColor")
+//                                ll_animal_detail.setBackgroundColor(intColor)
+////                                ll_animal_detail?.let {
+////                                    //it.setBackgroundColor()
+////                                }
+//
+//                            }
+                    }
+
+                    override fun onLoadCleared(placeholder: Drawable?) {
+
+
+                    }
+
+                })
+        }
+    }
 }
